d8a4b87adcb9dd10757cc239c5bbcb94565790aa
Ubuntu <ubuntu@ip-172-31-3-246.ap-south-1.compute.internal>
2026-08-01 19:06:52 +0000

    added docker compose

--- pavan-cricket-shop/app-tier/src/main/java/app/CricketProductRepository.java
+++ pavan-cricket-shop/app-tier/src/main/java/app/CricketProductRepository.java
@@ -1,6 +1,5 @@
-package com.cricketshop.repository;
+package app;
 
-import com.cricketshop.model.CricketProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CricketProductRepository extends JpaRepository<CricketProduct, Long> {
--- pavan-cricket-shop/app-tier/src/main/java/app/CricketShopApplication.java
+++ pavan-cricket-shop/app-tier/src/main/java/app/CricketShopApplication.java
@@ -1,11 +1,11 @@
-package com.cricketshop;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-SpringBootApplication
+@SpringBootApplication
 public class CricketShopApplication {
-	public static void main(string[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(CricketShopApplication.class, args);
 	}
 }
